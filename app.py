cf7d31553e54a6dc33ae9b1cdd5b7629b70fdd3e
bayaramine <bayaramine@users.noreply.github.com>
2026-04-08 12:24:53 +0200

    Use environment variable for secret key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'biospark-dev-key-change-in-production'
+app.secret_key = os.getenv('SECRET_KEY', 'biospark-dev-key-local')
 
 DATABASE = 'biospark.db'
 
